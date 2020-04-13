17aeba2a3775c0b849b7da9499997d6b92a4908e
ZumbaMaster313 <32375483+ZumbaMaster313@users.noreply.github.com>
2020-04-13 00:17:18 -0700

    🌽 umm

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const path = require('path');
+const request = require('request');
 const cheerio = require("cheerio");
-const axios = require('axios');
-var bodyParser = require("body-parser");
+const parseString = require('xml2js').parseString;
 const app = express();
 const hostname = '127.0.0.1';
 const port = 8080;
 const router = express.Router();
-
-var search = "https://www.bing.com/";
-var query = "search?q=";
 
 if (app.get('env') === 'development') {
     app.locals.pretty = true;
@@ -21,27 +18,36 @@ router.get('/', function(req, res) {
 
 router.get('/result', function(req, res) {
     var location = req.query.location;
-    var searchQuery = search + query + "food+banks+" + location;
-
-    axios(searchQuery)
-        .then(response => {
-            const html = response.data;
-            const text = html.text();
-
-            const $ = cheerio.load(text);
-            $('a').each((i, id) => {
-                const href = link.attribs.href;
-                console.log(href);
-            });
-            console.log(text);
-        })
-        .catch(console.error);
+    queryA = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
+    queryB = '&key=AIzaSyChcI4CFgqLT1w-kmzJXotlA03pPHKjiqI';
+    var query = queryA + 'food+banks+' + location + queryB;
 
 
+    request(query, {json: true}, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        const json = JSON.stringify(body);
+        const obj = JSON.parse(json);
 
- 
+        var output = '';
+
+
+        for (var i = 0; i < obj['results'].length; i++){
+            var bit = obj['results'][i];
+            output += '[formatted_address: "' + bit['formatted_address'] +
+            '" name: "' + bit['name'] +
+            '" rating: "' + bit['rating'] +
+            '" location : "' + bit['geometry'] +
+        '   ]\n';
+        };
+
+        console.log(output);
+
+    })
 
 });
+
 
 app.use('/', router);
 
