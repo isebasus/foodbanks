28c3e2f0f556a8ec3421ac901444352d02779c12
ZumbaMaster313 <32375483+ZumbaMaster313@users.noreply.github.com>
2020-04-13 19:18:20 -0700

    🥩 oops

--- index.js
+++ index.js
@@ -17,10 +17,15 @@ router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
+router.get('/banks', function(req, res) {
+    res.sendFile(path.join(__dirname+'/public/result.html'));
+});
+
 router.get('/result', function(req, res) {
     var location = req.query.location;
+
     queryA = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
-    queryB = '&key=AIzaSyChcI4CFgqLT1w-kmzJXotlA03pPHKjiqI';
+    queryB = '&key=KEY';
     var query = queryA + 'food+banks+' + location + queryB;
 
     let request = http.get(query, {json: true}, function(response){
@@ -73,53 +78,6 @@ router.get('/result', function(req, res) {
     })
     request.end();
 
-    /*
-    request(query, {json: true}, (err, res, body) => {
-        if (err) {
-            return console.log(err);
-        }
-
-
-        const json = JSON.stringify(body);
-        const obj = JSON.parse(json);
-
-        var output = [];
-
-        var result = [];
-        for(var i in obj){
-            result.push(obj['results']);
-        }
-       
-        var dataArr = [];
-        for (var i = 0; i < result.length; i++){
-            dataArr = result[i];
-        }
-
-        for (var i = 0; i < dataArr.length; i++){
-            var makeAr = [];
-            var open = [];
-
-            var object = {}
-            object["address"] = dataArr[i].formatted_address;
-            object["name"] = dataArr[i].name;
-            object["rating"] = dataArr[i].rating;
-            object['latitude'] = dataArr[i].geometry.location.lat;
-            object['longitude'] = dataArr[i].geometry.location.lng;
-
-            open.push(dataArr[i].opening_hours);
-            for(var z = 0; z < open.length; z++){
-                try{
-                    object["open"] = open[0].open_now;
-                }
-                catch (e) {object["open"] = "no information"}
-            }
-
-            makeAr.push(object);
-            output.push(makeAr);
-        }
-        res.
-    });
-    console.log(jsonResponse);*/
 });
 
 
