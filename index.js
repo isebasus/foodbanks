41a78a648c8563739661912718a895ec0f927a86
ZumbaMaster313 <32375483+ZumbaMaster313@users.noreply.github.com>
2020-04-20 10:33:35 -0700

    🥔 updating query

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ router.get('/result', function(req, res) {
     
     queryA = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
     queryB = '&key=AIzaSyChcI4CFgqLT1w-kmzJXotlA03pPHKjiqI';
-    var query = queryA + 'food+banks+' + location + queryB;
+    var query = queryA + 'foodbanks+' + location + queryB;
 
     let request = https.get(query, {json: true}, function(response){
         let jsonResponse = '';
