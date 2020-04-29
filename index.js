0d63604010be381497ea49edcb62f4c487e3e0bf
ZumbaMaster313 <32375483+ZumbaMaster313@users.noreply.github.com>
2020-04-29 12:43:47 -0700

    🌐 fixes

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path');
 var bodyParser = require("body-parser")
 const https = require('https');
 const http = require('http');
+const axios = require('axios');
 const app = express();
 var EventEmitter = require('events').EventEmitter;
 const hostname = '127.0.0.1';
 const port = 8080;
 const router = express.Router();
-
 
 app.set('view engine', 'ejs');
 
@@ -57,7 +57,6 @@ router.get('/banks', function(req, res) {
 
 router.get('/result', function(req, res) {
     var location = req.query.location;
-
     var parseLocation = location.substring(0, 2);
     
     queryA = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
@@ -70,7 +69,8 @@ router.get('/result', function(req, res) {
             jsonResponse += chunk;
         });
         response.on('end', function() {
-            const obj = JSON.parse(jsonResponse);
+            var obj = JSON.parse(jsonResponse);
+            var event = new EventEmitter();
 
             var output = [];
 
@@ -89,19 +89,24 @@ router.get('/result', function(req, res) {
 
                 var types = dataArr[i].types;
                 var address = dataArr[i].formatted_address;
+                var placeId = dataArr[i].place_id;
 
                 if(types.includes("atm") || types.includes("convenience_store") || types.includes("finance")){
                     dataArr.splice(index, 1);
                 } else if (address.includes(parseLocation) && address.includes("United States")){
+
+                    var query = "https://maps.googleapis.com/maps/api/place/details/json?place_id=" + placeId + "&fields=name,website,formatted_phone_number&key=AIzaSyChcI4CFgqLT1w-kmzJXotlA03pPHKjiqI";
+                    
+                    var object = {};
                     var open = [];
 
-                    var object = {}
+                    object["type"] = types[0];
                     object["address"] = address;
                     object["name"] = dataArr[i].name;
                     object["rating"] = dataArr[i].rating;
                     object['latitude'] = dataArr[i].geometry.location.lat;
                     object['longitude'] = dataArr[i].geometry.location.lng;
-    
+            
                     open.push(dataArr[i].opening_hours);
                     for(var z = 0; z < open.length; z++){
                         try{
@@ -109,13 +114,45 @@ router.get('/result', function(req, res) {
                         }
                         catch (e) {object["open"] = "no information"}
                     }
-    
+                    
+                    axios.get(query).then(response => {
+                        var getData = [];
+                        getData.push(response.data['result']);
+                        event.getData = getData;
+                        event.emit('update');
+                    }).catch(error => {
+                        console.log(error);
+                    });
+                
                     output.push(object);
-                }
 
+                }
                 index++;
             }
-            res.send(output);
+            var increment = 0;
+            var index = 1;
+            event.on('update', function(){
+                var dataResponse = event.getData;
+                var details = dataResponse[0];
+
+                for(var i = 0; i < output.length; i++){    
+                    if(output[i].name == details.name){
+                        try{ output[i]['website'] = details.website; }
+                        catch (e){ output[i]['website'] = ""; }
+        
+                        try{ output[i]['phoneNumber'] = details.formatted_phone_number; }
+                        catch (e){ output[i]['phoneNumber'] = ""; }
+                    }
+                
+                }
+                if (output.length == index){
+                    res.send(output);
+                }
+
+                increment++;
+                index++;
+            });
+
         })
     });
     request.on('error', function(error) {
