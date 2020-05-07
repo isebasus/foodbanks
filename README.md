7927641c615ad9a3f18be976e7b0b441f9fee98e
ZumbaMaster313 <32375483+ZumbaMaster313@users.noreply.github.com>
2020-05-06 18:25:07 -0700

    📋 ok just a little update

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-## 🛍️ Grocery
+# 🛍️ Grocery
 
 A website where individuals financially struggling with the coronavirus can find food 🥔. 
 
-![Alt text](https://github.com/ZumbaMaster313/Grocery/blob/master/public/images/foodbanks.JPG "Proxy")
+## 📋 Features 
+
+* Find food banks
+* Get details
+* Compare Banks
+
+## 📍 About
+
+I thought it would be nice if I could help people struggling from the corona-virus pandemic. Through these harsh times, it's hard for financially deprived individuals to find food. So I created a website to locatte foodbanks near them. 
+
+![Alt text](https://github.com/ZumbaMaster313/Grocery/blob/master/public/images/foodbanksB.JPG "Proxy")
--- index.js
+++ index.js
@@ -21,50 +21,67 @@ if (app.get('env') === 'development') {
 }
 
 router.get('/', function(req, res) {
-    res.render("index");
+    res.render("index", {error: ""});
 });
 
 router.get('/banks', function(req, res) {
     var location = req.query.location;
+    var check = location.split("");
 
-    setTimeout(() => {
-        var responseData = new EventEmitter();
+    var isZip = false;
+    for(var i = 0; i < check.length; i++){
+        if(/^\d+$/.test(check[i])){
+            isZip = true;
+        }
+        else{
+            isZip = false;
+        }
+    }
 
-        var query = "http://localhost:8080/result?location=" + location;
-
-        let request = http.get(query, {json: true}, function(response){
-            let jsResponse = '';
-            response.on('data', function(chunk) {
-                jsResponse += chunk;
+    if(isZip && check.length == 5){
+        setTimeout(() => {
+            var responseData = new EventEmitter();
+    
+            var query = "http://localhost:8080/result?location=" + location;
+    
+            let request = http.get(query, {json: true}, function(response){
+                let jsResponse = '';
+                response.on('data', function(chunk) {
+                    jsResponse += chunk;
+                });
+                response.on('end', function(){
+                    var obj = JSON.parse(jsResponse);
+                    responseData.obj = obj;
+                    responseData.emit('update');
+                });
             });
-            response.on('end', function(){
-                var obj = JSON.parse(jsResponse);
-                responseData.obj = obj;
-                responseData.emit('update');
+            request.on('error', function(error) {
+                console.log(error)
             });
-        });
-        request.on('error', function(error) {
-            console.log(error)
-        });
-        request.end();
-
-        responseData.on('update', function(){
-            var dataResponse = JSON.stringify(responseData.obj);
-            res.render("result", {loc: location, data: dataResponse});
-        });
-
-    }, 1000);
+            request.end();
+    
+            responseData.on('update', function(){
+                var dataResponse = JSON.stringify(responseData.obj);
+                res.render("result", {loc: location, data: dataResponse});
+            });
+    
+        }, 1000);
+    }
+    else{
+        res.render("index", {error: "Please enter a zip code"});
+    }
 
 });
 
 router.get('/result', function(req, res) {
     var location = req.query.location;
+
     var parseLocation = location.substring(0, 2);
 
     queryA = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
     queryB = '&key=AIzaSyChcI4CFgqLT1w-kmzJXotlA03pPHKjiqI';
     var query = queryA + 'food+banks+' + location + queryB;
-
+    
     let request = https.get(query, {json: true}, function(response){
         let jsonResponse = '';
         response.on('data', function(chunk) {
@@ -73,42 +90,42 @@ router.get('/result', function(req, res) {
         response.on('end', function() {
             var obj = JSON.parse(jsonResponse);
             var event = new EventEmitter();
-
+    
             var output = [];
-
+    
             var result = [];
             for(var i in obj){
                 result.push(obj['results']);
             }
-       
+           
             var dataArr = [];
             for (var i = 0; i < result.length; i++){
                 dataArr = result[i];
             }
-
+    
             var index = 0;
             for (var i = 0; i < dataArr.length; i++){
-
+    
                 var types = dataArr[i].types;
                 var address = dataArr[i].formatted_address;
                 var placeId = dataArr[i].place_id;
-
+    
                 if(types.includes("atm") || types.includes("convenience_store") || types.includes("finance")){
                     dataArr.splice(index, 1);
                 } else if (address.includes(parseLocation) && address.includes("United States")){
-
+    
                     var query = "https://maps.googleapis.com/maps/api/place/details/json?place_id=" + placeId + "&fields=name,website,formatted_phone_number&key=AIzaSyChcI4CFgqLT1w-kmzJXotlA03pPHKjiqI";
-                    
+                        
                     var object = {};
                     var open = [];
-
+    
                     object["type"] = types[0];
                     object["address"] = address;
                     object["name"] = dataArr[i].name;
                     object["rating"] = dataArr[i].rating;
                     object['latitude'] = dataArr[i].geometry.location.lat;
                     object['longitude'] = dataArr[i].geometry.location.lng;
-            
+                
                     open.push(dataArr[i].opening_hours);
                     for(var z = 0; z < open.length; z++){
                         try{
@@ -116,7 +133,7 @@ router.get('/result', function(req, res) {
                         }
                         catch (e) {object["open"] = "no information"}
                     }
-                    
+                        
                     axios.get(query).then(response => {
                         var getData = [];
                         getData.push(response.data['result']);
@@ -125,9 +142,9 @@ router.get('/result', function(req, res) {
                     }).catch(error => {
                         console.log(error);
                     });
-                
+                    
                     output.push(object);
-
+    
                 }
                 index++;
             }
@@ -136,35 +153,32 @@ router.get('/result', function(req, res) {
             event.on('update', function(){
                 var dataResponse = event.getData;
                 var details = dataResponse[0];
-
+    
                 for(var i = 0; i < output.length; i++){    
                     if(output[i].name == details.name){
                         try{ output[i]['website'] = details.website; }
                         catch (e){ output[i]['website'] = ""; }
-        
+            
                         try{ output[i]['phoneNumber'] = details.formatted_phone_number; }
                         catch (e){ output[i]['phoneNumber'] = ""; }
                     }
-                
+                    
                 }
                 if (output.length == index){
                     res.send(output);
                 }
-
+    
                 increment++;
                 index++;
             });
-
+    
         })
     });
     request.on('error', function(error) {
         console.log(error)
     })
     request.end();
-
-    function hasNumber(searchQuery){
-        return /\d/.test(searchQuery);
-    }
+    
 
 });
 
